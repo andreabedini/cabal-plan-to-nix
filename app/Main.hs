b809bbaa003133c5a20869e1aab95efb4b7f2214
Andrea Bedini <andrea@andreabedini.com>
2022-10-13 18:12:42 +0800

    More

--- app/Main.hs
+++ app/Main.hs
@@ -20,22 +20,24 @@ import Distribution.Client.InstallPlan qualified as InstallPlan
 import Distribution.Client.NixStyleOptions (NixStyleFlags (..), defaultNixStyleFlags, nixStyleOptions)
 import Distribution.Client.PackageHash
 import Distribution.Client.ProjectConfig
-import Distribution.Client.ProjectPlanning
-  ( rebuildInstallPlan,
-    rebuildProjectConfig,
-  )
+import Distribution.Client.ProjectPlanning (rebuildInstallPlan, rebuildProjectConfig)
 import Distribution.Client.ProjectPlanning.Types
-import Distribution.Client.Setup
+import Distribution.Client.Setup hiding (cabalVersion)
 import Distribution.Client.Types.ConfiguredId
 import Distribution.Client.Types.PackageLocation
 import Distribution.Client.Types.Repo
 import Distribution.Client.Types.SourceRepo
+import Distribution.Client.Version (cabalInstallVersion)
 import Distribution.Compat.Directory (makeAbsolute)
-import Distribution.PackageDescription qualified as PD
+import Distribution.InstalledPackageInfo (InstalledPackageInfo)
+import Distribution.Package (HasUnitId (installedUnitId), installedDepends, packageId)
+import Distribution.PackageDescription
 import Distribution.Pretty (Pretty, prettyShow)
 import Distribution.Simple.Command
+import Distribution.Simple.Compiler (showCompilerId)
 import Distribution.Simple.Flag
-import Distribution.Types.PackageId
+import Distribution.Simple.Utils
+import Distribution.System
 import Distribution.Verbosity (moreVerbose)
 import Distribution.Verbosity qualified as Verbosity
 import Network.URI (URI (uriPath))
@@ -100,10 +102,10 @@ installPlanAction verbosity cliConfig = do
   -- packages are replaced by pre-existing installed packages from the
   -- store (when their ids match), and also the original elaborated plan
   -- which uses primarily source packages.
-  (_improvedPlan, elaboratedPlan, _elaboratedSharedConfig, _tis, _at) <-
+  (_improvedPlan, elaboratedPlan, elaboratedShared, _tis, _at) <-
     rebuildInstallPlan verbosity distDirLayout cabalDirLayout projectConfig localPackages
 
-  print $ prettyNix $ makeNixPlan elaboratedPlan
+  print $ prettyNix $ makeNixPlan elaboratedPlan elaboratedShared
 
 cabalDirLayoutFromProjectConfig :: ProjectConfig -> IO CabalDirLayout
 cabalDirLayoutFromProjectConfig
@@ -122,47 +124,66 @@ cabalDirLayoutFromProjectConfig
 
     return $ mkCabalDirLayout cabalDir mstoreDir (flagToMaybe projectConfigLogsDir)
 
-makeNixPlan :: ElaboratedInstallPlan -> NExpr
-makeNixPlan elaboratedInstallPlan =
+makeNixPlan :: ElaboratedInstallPlan -> ElaboratedSharedConfig -> NExpr
+makeNixPlan elaboratedInstallPlan elaboratedShared =
   "srp"
     ==> "self"
     ==> mkNonRecSet
-      [ "install-plan" $= mkNonRecSet pkgs
+      [ "cabal-install-version" $= mkStr (display cabalInstallVersion),
+        "cabal-version" $= mkStr (display cabalVersion),
+        "compiler-id" $= mkStr (T.pack $ showCompilerId $ pkgConfigCompiler elaboratedShared),
+        "os" $= mkStr (display os),
+        "arch" $= mkStr (display arch),
+        "install-plan" $= installPlanToNix (InstallPlan.toList elaboratedInstallPlan)
       ]
   where
-    pkgs = [elaboratedPackageToNix ecp | InstallPlan.Configured ecp <- InstallPlan.toList elaboratedInstallPlan]
+    (Platform arch os) = pkgConfigPlatform elaboratedShared
 
-    elaboratedPackageToNix :: ElaboratedConfiguredPackage -> Binding NExpr
-    elaboratedPackageToNix
-      ecp@ElaboratedConfiguredPackage
-        { elabUnitId,
-          elabPkgSourceId = PackageIdentifier {pkgName, pkgVersion},
-          elabFlagAssignment,
-          elabPkgSourceLocation,
-          elabPkgSourceHash,
-          elabPkgDescriptionOverride
-        } =
-        T.pack (quoted $ prettyShow elabUnitId)
-          $= mkNonRecSet
-            ( catMaybes
-                [ Just $ "pkg-name" $= mkStr (prettyShowText pkgName),
-                  Just $ "pkg-version" $= mkStr (prettyShowText pkgVersion),
-                  Just $ "pkg-src" $= packageLocationToNix elabPkgSourceLocation elabPkgSourceHash,
-                  case flagAssignmentToNix elabFlagAssignment of
-                    fs | fs == emptySet -> Nothing
-                    fs -> Just ("flags" $= fs),
-                  case elabLibDependencies ecp of
-                    [] -> Nothing
-                    libdeps -> Just $ "lib-depends" $= mkList ["self" @. quoted (prettyShowText $ confInstId cid) | cid <- libdeps],
-                  case elabExeDependencies ecp of
-                    [] -> Nothing
-                    exedeps -> Just $ "exe-depends" $= mkList ["self" @. quoted (prettyShowText cid) | cid <- exedeps],
-                  case elabSetupDependencies ecp of
-                    [] -> Nothing
-                    setupdeps -> Just $ "setup-depends" $= mkList ["self" @. quoted (prettyShowText $ confInstId cid) | cid <- setupdeps],
-                  (\pdo -> "pkg-cabal-file" $= mkIndentedStr 4 (T.decodeUtf8 $ BL.toStrict pdo)) <$> elabPkgDescriptionOverride
-                ]
-            )
+    installPlanToNix :: [ElaboratedPlanPackage] -> NExpr
+    installPlanToNix pkgs = mkNonRecSet [displayQuoted (installedUnitId pkg) $= go pkg | pkg <- pkgs]
+      where
+        go pkg = case pkg of
+          InstallPlan.PreExisting ipi -> installedPackageInfoToNix ipi
+          InstallPlan.Configured elab -> elaboratedPackageToNix False elab
+          InstallPlan.Installed elab -> elaboratedPackageToNix True elab
+
+    installedPackageInfoToNix :: InstalledPackageInfo -> NExpr
+    installedPackageInfoToNix ipi =
+      mkNonRecSet $
+        catMaybes
+          [ Just $ "type" $= mkStr "pre-existing",
+            Just $ "id" $= mkStr (display $ installedUnitId ipi),
+            Just $ "pkg-id" $= mkStr (display $ packageId ipi),
+            case installedDepends ipi of
+              [] -> Nothing
+              libdeps -> Just $ "depends" $= mkList ["self" @. displayQuoted uid | uid <- libdeps]
+          ]
+
+    elaboratedPackageToNix :: Bool -> ElaboratedConfiguredPackage -> NExpr
+    elaboratedPackageToNix isInstalled elab =
+      mkNonRecSet $
+        catMaybes
+          [ Just $ "type" $= mkStr (if isInstalled then "installed" else "configured"),
+            Just $ "pkg-id" $= mkStr (display $ packageId elab),
+            Just $ "pkg-src" $= packageLocationToNix (elabPkgSourceLocation elab) (elabPkgSourceHash elab),
+            case flagAssignmentToNix (elabFlagAssignment elab) of
+              fs | fs == emptySet -> Nothing
+              fs -> Just ("flags" $= fs),
+            case elabLibDependencies elab of
+              [] -> Nothing
+              libdeps -> Just $ "lib-depends" $= mkList ["self" @. displayQuoted (confInstId cid) | cid <- libdeps],
+            case elabExeDependencies elab of
+              [] -> Nothing
+              exedeps -> Just $ "exe-depends" $= mkList ["self" @. displayQuoted cid | cid <- exedeps],
+            case elabSetupDependencies elab of
+              [] -> Nothing
+              setupdeps -> Just $ "setup-depends" $= mkList ["self" @. displayQuoted (confInstId cid) | cid <- setupdeps],
+            case elabPkgConfigDependencies elab of
+              [] -> Nothing
+              pkgConfigDeps ->
+                Just $ "pkg-condig-depends" $= mkList [pkgconfigDependsToNix n mv | (n, mv) <- pkgConfigDeps],
+            (\pdo -> "pkg-cabal-file" $= mkIndentedStr 4 (T.decodeUtf8 $ BL.toStrict pdo)) <$> elabPkgDescriptionOverride elab
+          ]
 
 packageLocationToNix :: PackageLocation (Maybe FilePath) -> Maybe PackageSourceHash -> NExpr
 packageLocationToNix (RepoTarballPackage (RepoLocalNoIndex _lr _s) _pkgId _local) (Just _srcHash) =
@@ -183,7 +204,7 @@ packageLocationToNix (RemoteSourceRepoPackage srcRepo _) (Just srcHash) =
       "srp"
         @@ mkNonRecSet
           ( catMaybes
-              [ Just $ "type" $= mkStr (prettyShowText repoType),
+              [ Just $ "type" $= mkStr (display repoType),
                 Just $ "location" $= mkStr (T.pack srpLocation),
                 Just $ "sdist-sha256" $= mkStr (T.pack $ showHashValue srcHash),
                 (\b -> "branch" $= mkStr (T.pack b)) <$> srpBranch,
@@ -193,6 +214,14 @@ packageLocationToNix (RemoteSourceRepoPackage srcRepo _) (Just srcHash) =
           )
 packageLocationToNix _ Nothing =
   error "No source hash available"
+
+pkgconfigDependsToNix :: PkgconfigName -> Maybe PkgconfigVersion -> NExpr
+pkgconfigDependsToNix n mv =
+  mkNonRecSet $
+    catMaybes
+      [ Just $ "name" $= mkStr (displayQuoted n),
+        fmap (\v -> "version" $= mkStr (display v)) mv
+      ]
 
 fetchRemoteRepoPackage :: RemoteRepo -> PackageId -> PackageSourceHash -> NExpr
 fetchRemoteRepoPackage RemoteRepo {remoteRepoURI} pkgId =
@@ -211,12 +240,15 @@ fetch uri hash =
         "sha256" $= mkStr (T.pack $ showHashValue hash)
       ]
 
-flagAssignmentToNix :: PD.FlagAssignment -> NExpr
+flagAssignmentToNix :: FlagAssignment -> NExpr
 flagAssignmentToNix flagAssignment =
-  mkNonRecSet [T.pack (prettyShow fn) $= mkBool fv | (fn, fv) <- PD.unFlagAssignment flagAssignment]
+  mkNonRecSet [display fn $= mkBool fv | (fn, fv) <- unFlagAssignment flagAssignment]
 
 quoted :: (IsString a, Semigroup a) => a -> a
 quoted str = "\"" <> str <> "\""
 
-prettyShowText :: Pretty a => a -> T.Text
-prettyShowText = T.pack . prettyShow
+display :: Pretty a => a -> T.Text
+display = T.pack . prettyShow
+
+displayQuoted :: Pretty a => a -> T.Text
+displayQuoted = T.pack . quoted . prettyShow
